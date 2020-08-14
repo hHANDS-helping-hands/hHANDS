f8f2cf3ac5ebcce887975cd00c06bdb7b62527a2
Your Name <you@example.com>
2020-08-14 11:11:30 +0530

    Delete Ticket has been added

--- utilities/Config.js
+++ utilities/Config.js
@@ -1,6 +1,6 @@
 const Config = {
-  server: "http://159.65.158.206:8080",
-  //server: "http://192.168.29.82:8080",
+  //server: "http://159.65.158.206:8080",
+  server: "http://192.168.29.82:8080",
   initialLocation: {
     latitude: 18.5204,
     longitude: 73.8567,
